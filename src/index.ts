ff3b7db10a790d95401c01c2faeca5c1108cdc4d
thenamelessdev <aron.minarovits@gmail.com>
2025-12-18 21:23:05 +0100

    added message feature

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,32 @@ export function config(token:string, errorChannelId:string){
     configs.errorChannelId = errorChannelId;
     confStat = true;
 }
+
+export async function sendError(error:string){
+    if(!confStat){
+        throw new Error("Missing config");
+    }
+    try{
+        const response = await fetch(`https://discord.com/api/v10/channels/${configs.errorChannelId}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bot ${configs.token}`
+            },
+            body: JSON.stringify({
+                embeds: [
+                    {
+                        title: "Error",
+                        description: error
+                    }
+                ]
+            })
+        });
+        if(!response.ok){
+            throw new Error(`There was an error in the api request. Status code: ${response.status}`);
+        }
+    }
+    catch{
+        throw new Error("There was an error while sending the api request");
+    }
+}
---
+++ src/messages.ts
@@ -0,0 +1,27 @@
+import { confStat, configs, sendError } from "./index.js";
+
+export async function sendMessage(channelId:string, message:string){
+    if(!confStat){
+        throw new Error("Missing config");
+    }
+    try{
+        const response = await fetch(`https://discord.com/api/v10/channels/${channelId}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bot ${configs.token}`
+            },
+            body: JSON.stringify({
+                content: message
+            })
+        });
+        if(!response.ok){
+            sendError(`There was an error while sending the api request. Status code: ${response.status}`);
+            throw new Error(`There was an error while sending the api request. Status code: ${response.status}`);
+        }
+    }
+    catch{
+        sendError("There was an error while sending the api request.");
+        throw new Error("There was an error while sending the api request.");
+    }
+}
