1c3474ef970481f420a7a7b1cb286a5a04eef848
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-12-19 09:11:13 +0100

    added deleteWebhook function

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -18,3 +18,17 @@ export async function executeWebhook(url:string, message?:string, embeds?:any) {
         throw new Error("There was an error while sending the api request");
     }
 }
+
+export async function deleteWebhoook(url:string) {
+    try{
+        const response = await fetch(url, {
+            method: "DELETE"
+        });
+        if(!response.ok){
+            throw new Error(`There was an error while sending the api request. Status code: ${response.status}`);
+        }
+    }
+    catch{
+        throw new Error("There was an error while sending the api request");
+    }
+}
