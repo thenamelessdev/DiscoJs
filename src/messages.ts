309d95853757ed61cb03ba81afa3e3a94cf71062
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-12-20 22:12:09 +0100

    added pin and unpin functions

--- src/messages.ts
+++ src/messages.ts
@@ -112,3 +112,53 @@ export async function editMessage(channelId: string, messageId:string, message?:
         throw new Error("There was an error while sending the api request.");
     }
 }
+
+export async function pinMessage(channelId: string, messageId:string) {
+    if (!confStat) {
+        throw new Error("Missing config");
+    }
+    try {
+        const response = await fetch(`https://discord.com/api/v10/channels/${channelId}/messages/${messageId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bot ${configs.token}`
+            }
+        });
+        if (!response.ok) {
+            await sendError(`There was an error while sending the api request. Status code: ${response.status}`);
+            throw new Error(`There was an error while sending the api request. Status code: ${response.status}`);
+        }
+        const json = await response.json();
+        return json;
+    }
+    catch (err) {
+        await sendError("There was an error while sending the api request.");
+        throw new Error("There was an error while sending the api request.");
+    }
+}
+
+export async function unpinMessage(channelId: string, messageId:string) {
+    if (!confStat) {
+        throw new Error("Missing config");
+    }
+    try {
+        const response = await fetch(`https://discord.com/api/v10/channels/${channelId}/messages/${messageId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bot ${configs.token}`
+            }
+        });
+        if (!response.ok) {
+            await sendError(`There was an error while sending the api request. Status code: ${response.status}`);
+            throw new Error(`There was an error while sending the api request. Status code: ${response.status}`);
+        }
+        const json = await response.json();
+        return json;
+    }
+    catch (err) {
+        await sendError("There was an error while sending the api request.");
+        throw new Error("There was an error while sending the api request.");
+    }
+}
