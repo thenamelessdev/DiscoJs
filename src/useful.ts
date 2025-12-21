2d30f12a0686c5edf788c27dbb0dd3295270f0b2
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-12-21 20:58:51 +0100

    added unauthed mode for api cinftion because i forgot

--- src/useful.ts
+++ src/useful.ts
@@ -2,7 +2,25 @@ import { confStat, configs } from "./config.js"
 
 export async function api(endpoint:string, method:string, auth:boolean, body?:any) {
     if(!auth){
-
+        try{
+            const response = await fetch(`https://discord.com${endpoint}`, {
+                method: method,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(body)
+            });
+            const json = await response.json();
+            if(response.ok){
+                return json;
+            }
+            else{
+                throw new Error(`There was an error while sending the api request. Status code: ${response.status}`);
+            }
+        }
+        catch{
+            throw new Error("There was an error while sending the api request");
+        }
     }
     else{
         if(!confStat){
