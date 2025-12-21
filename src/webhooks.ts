f11454235f22d008459b1b7a66c4e9215d40873a
Nameless <132124594+thenamelessdev@users.noreply.github.com>
2025-12-21 21:04:25 +0100

    added editWebhookMessage function and bug fix

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -13,7 +13,7 @@ export async function executeWebhook(url:string, message?:string, embeds?:any) {
         if(!response.ok){
             throw new Error(`There was an error while sending the api request. Status code: ${response.status}`);
         }
-        return response.json();
+        return await response.json();
     }
     catch{
         throw new Error("There was an error while sending the api request");
@@ -31,5 +31,29 @@ export async function deleteWebhoook(url:string) {
     }
     catch{
         throw new Error("There was an error while sending the api request");
+    }
+}
+
+export async function editWebhookMessage(url:string, messageId:string, message?:string, embeds?: any) {
+    try{
+        const response = await fetch(`${url}/messages/${messageId}`, {
+            method: "PATCH",
+            headers: {
+                "Conent-Type": "application/json"
+            },
+            body: JSON.stringify({
+                content: message,
+                embeds: embeds
+            })
+        });
+        if(response.ok){
+            return await response.json();
+        }
+        else{
+            throw new Error(`There was an error while sending the api request. Status code: ${response.status}`);
+        }
+    }
+    catch{
+        throw new Error(`There was an error while sending the api request`);
     }
 }
